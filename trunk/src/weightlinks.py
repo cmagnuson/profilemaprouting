ac65db5401ef9df236b80c7cba6006cc61e3234a
magnuson.carl <magnuson.carl@43829d72-31b4-11de-a1c6-114bfcb2beae>
2009-08-12 19:02:56 +0000

    Default link weighting added in very basic form, fixed simple weighting method.

--- trunk/src/weightlinks.py
+++ trunk/src/weightlinks.py
@@ -21,7 +21,7 @@ def main():
     p.add_option('--verbose', '-v', action="store_true", default=False)
     options, arguments = p.parse_args()
 
-        
+    #TODO: try other db drivers to get something Python 2.x compatible.
     #TODO: move db code to separate module
     #connect to database and prepare statements
     #TODO: more informative error code for say wrong db, username, duplicated tracks, etc
@@ -29,16 +29,35 @@ def main():
                          options.db_ip+"/"+options.db_name)
     db.connect()
 
+    transaction = db.xact()
+    transaction.start()
+
+    clear_weights = db.prepare("""UPDATE ways SET to_cost=length/1""")
+    clear_weights()
+
+    if options.verbose:
+        print("Roadway weightings cleared")
+        
+    #TODO: read this from a config file or something similar
+    weights = [(100,1),(101,55),(102,30),(104,40),(105,25),(106,35),(107,25),(108,25),(109,25),(201,30)]
+    
     #TODO: default speeds for roadways based on classification
-    #default_weights = db.prepare("....)
-    simple_weight = db.prepare("""UPDATE ways SET to_cost=spavg.sp*length FROM (SELECT AVG(speed) AS sp, COUNT(gid) AS cnt, gid AS g FROM userdata WHERE gid IS NOT NULL GROUP BY gid) AS spavg
- WHERE gid=spavg.g AND spavg.cnt>10""")
+    default_weight = db.prepare("""UPDATE ways SET to_cost=length/$2 WHERE class_id=$1""")
+    for wt in weights:
+        default_weight(wt[0],wt[1])
+    
+    if options.verbose:
+        print("Default weights assigned")
+    
+    simple_weight = db.prepare("""UPDATE ways SET to_cost=length/spavg.sp FROM (SELECT AVG(speed) AS sp, COUNT(gid) AS cnt, gid AS g FROM userdata WHERE gid IS NOT NULL GROUP BY gid) AS spavg
+ WHERE gid=spavg.g AND spavg.cnt>10 AND spavg.sp>0""")
 
     simple_weight()
    
+    transaction.commit()
   
     if options.verbose:
-        print("Road network data assigned weights")
+        print("Road network data assigned user weights")
     
 
 if __name__ == '__main__':
