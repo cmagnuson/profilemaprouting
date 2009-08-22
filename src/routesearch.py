591405f14cda25179c82b8a651a08183b9b2255b
magnuson.carl <magnuson.carl@43829d72-31b4-11de-a1c6-114bfcb2beae>
2009-08-22 05:02:35 +0000

    Output of driving distance and time to console.

--- src/routesearch.py
+++ src/routesearch.py
@@ -41,6 +41,7 @@ def main():
     search = db.prepare("""SELECT gid, ST_AsKML(the_geom) AS the_geom, AsText(the_geom) FROM dijkstra_sp_delta('ways', $1, $2, 0.1)""")
     get_gid = db.prepare("""SELECT find_nearest_link_within_distance(AsText(setsrid(makepoint($1,$2),4326)), .1, 'ways')""")
     get_node = db.prepare("""SELECT source FROM ways WHERE gid=$1""")
+    get_dist = db.prepare("""SELECT length, to_cost, name FROM ways WHERE gid=$1""")
     
     gid = get_gid(float(src_lon), float(src_lat))
     src = get_node(gid[0][0])
@@ -50,15 +51,23 @@ def main():
 
 
     route = search(src[0][0], dst[0][0])
-    print(route)
+    #print(route)
+    total_distance = 0
+    total_time = 0
     
     kmlfile = open("search.kml","w")
     kmlfile.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n")
     kmlfile.write("<kml xmlns=\"http://www.opengis.net/kml/2.2\">\n")
     kmlfile.write("<Folder>\n")
     kmlfile.write("<Style id=\"trkStyle\"><LineStyle><color>ffff0000</color><width>4</width></LineStyle><PolyStyle><color>7f00ff00</color></PolyStyle></Style>\n")
+    print("Road | Cumulative Distance | Cumulative Time")
 
     for link in route:
+        roadinfo = get_dist(link[0])[0]
+        total_distance += float(roadinfo[0])
+        total_time += float(roadinfo[1]*60)
+        print(roadinfo[2]+" | "+str(total_distance)+" | "+str(total_time))
+        
         kmlfile.write("<Placemark>\n")
         kmlfile.write(link[1])
         kmlfile.write("\n</Placemark>\n")
